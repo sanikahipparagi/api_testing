c3f757fc32c4e9f5f50247e1e32483d1bb7969f5
Sanika Hipparagi <hippmanohar1@gmail.com>
2023-08-15 00:20:53 +0530

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    email : String,
+    password: String,
+    token : {
+        type:String,
+        default:''
+    }
+});
+
+const User = mongoose.model('User',UserSchema);
+
+
+
+module.exports = User;
